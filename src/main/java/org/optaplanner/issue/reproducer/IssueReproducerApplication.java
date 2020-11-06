c2ab3afee5ca1e7585725b933e36f4aba879613e
Christopher Chianelli <chris.chianelli@gmail.com>
2020-11-06 11:16:10 -0500

    Create base setup for issue reproducers

---
+++ src/main/java/org/optaplanner/issue/reproducer/IssueReproducerApplication.java
@@ -0,0 +1,17 @@
+package org.optaplanner.issue.reproducer;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+import io.quarkus.runtime.StartupEvent;
+
+@ApplicationScoped
+public class IssueReproducerApplication {
+    private static final Logger LOGGER = Logger.getLogger("ListenerBean");
+
+    void onStart(@Observes StartupEvent ev) {
+        // Create reproducer here
+    }
+}
