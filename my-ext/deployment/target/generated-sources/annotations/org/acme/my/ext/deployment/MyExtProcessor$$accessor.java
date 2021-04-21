ee2f87221fabb8f0a4082aef4c5d8045e648fe08
Christopher Chianelli <chris.chianelli@gmail.com>
2021-04-21 14:08:41 -0400

    Add reproducer for Dev UI ignoring local extension embedded.html

---
+++ my-ext/deployment/target/generated-sources/annotations/org/acme/my/ext/deployment/MyExtProcessor$$accessor.java
@@ -0,0 +1,7 @@
+package org.acme.my.ext.deployment;
+public final class MyExtProcessor$$accessor {
+    private MyExtProcessor$$accessor() {}
+    public static Object construct() {
+        return new MyExtProcessor();
+    }
+}
---
+++ my-ext/integration-tests/src/test/java/org/acme/my/ext/it/NativeMyExtResourceIT.java
@@ -0,0 +1,7 @@
+package org.acme.my.ext.it;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMyExtResourceIT extends MyExtResourceTest {
+}
