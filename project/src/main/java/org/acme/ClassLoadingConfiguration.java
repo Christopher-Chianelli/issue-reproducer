f852e3aefd5b17680008d7abb883e2c17ec06159
Christopher Chianelli <christopher@timefold.ai>
2024-02-08 11:46:30 -0500

    Add RegisterReflectionForBinding to prove registering classes for reflection fails to work

--- project/src/main/java/org/acme/ClassLoadingConfiguration.java
+++ project/src/main/java/org/acme/ClassLoadingConfiguration.java
@@ -1,9 +1,11 @@
 package org.acme;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RegisterReflectionForBinding(MyAnnotatedClass.class)
 public class ClassLoadingConfiguration {
     @Bean
     public static ClassLoadingPostProcessor getClassLoadingPostProcessor() {
